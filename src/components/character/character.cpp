e39da7157cbfb9c1893774537a0a963fe234334b
kesav5678 <venkata.kesav@students.iiit.ac.in>
2023-01-28 22:54:44 +0530

    Both Character and BackGround Seperated

---
+++ src/components/character/character.cpp
@@ -0,0 +1,79 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include <iostream>
+#include "./../../shaders/shader.h"
+#ifndef STB_IMAGE_IMPLEMENTATION
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>
+#endif
+
+float vertices_1[] = {
+    // Position (x, y, z)  //Colors (arbitrary)  //Textures(x, y)
+    -0.80f, -0.60f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f,
+    -0.80f, -0.93f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f,
+    -0.90f, -0.93f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f,
+    -0.90f, -0.60f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f};
+unsigned int indices_1[] = {
+    0, 1, 3, // second triangle
+    1, 2, 3  // first triangle
+};
+
+unsigned int VBO_1, VAO_1, EBO_1;
+unsigned int texture_1;
+
+void character_init()
+{
+
+    glGenVertexArrays(1, &VAO_1);
+    glGenBuffers(1, &VBO_1);
+    glGenBuffers(1, &EBO_1);
+
+    glBindVertexArray(VAO_1);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO_1);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices_1), vertices_1, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO_1);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices_1), indices_1, GL_STATIC_DRAW);
+
+    // position attribute
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)0);
+    glEnableVertexAttribArray(0);
+    // color attribute
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+    // texture coord attribute
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)(6 * sizeof(float)));
+    glEnableVertexAttribArray(2);
+
+    // load and create a texture
+    // -------------------------
+    glGenTextures(1, &texture_1);
+    glBindTexture(GL_TEXTURE_2D, texture_1); // all upcoming GL_TEXTURE_2D operations now have effect on this texture object
+    // set the texture wrapping parameters
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT); // set texture wrapping to GL_REPEAT (default wrapping method)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    // set texture filtering parameters
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    // load image, create texture and generate mipmaps
+    // int width, height, nrChannels;
+    char path_1[100];
+    strcpy(path_1, "src/components/character/H1_F.png");
+    // The FileSystem::getPath(...) is part of the GitHub repository so we can find files on any IDE/platform; replace it with your own image path.
+    unsigned char *data_1 = stbi_load(path_1, &width, &height, &nrChannels, 0);
+    if (data_1)
+    {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data_1);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else
+    {
+        std::cout << "Failed to load texture" << std::endl;
+    }
+    stbi_image_free(data_1);
+}
--- src/hello_opengl.cpp
+++ src/hello_opengl.cpp
@@ -12,9 +12,16 @@
 #include <unistd.h>
 
 #include "./components/background/background.cpp"
+#include "./components/character/character.cpp"
+
 
 // void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow *window);
+
+int new_Time = 0;
+int flag = 0;
+
+int count = 0;
 
 int main()
 {
@@ -23,163 +30,10 @@ int main()
     // build and compile our shader program
     // ------------------------------------
     Shader ourShader("src/shaders/shader.vs", "src/shaders/shader.fs"); // you can name your shader files however you like
-
-    // // set up vertex data (and buffer(s)) and configure vertex attributes
-    // // ------------------------------------------------------------------
-    // // set up vertex data (and buffer(s)) and configure vertex attributes
-    // // ------------------------------------------------------------------
-    // // We fit in all the 4 frames into the function -> Apply this
-    // /*Code For Background*/
-    // float vertices[] = {
-    //     // positions          // colors           // texture coords
-    //     -0.33f, 1.0f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f,  // top right
-    //     -0.33f, -1.0f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f, // bottom right
-    //     -1.0f, -1.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f,  // bottom left
-    //     -1.0f, 1.0f, 0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f,   // top left
-    //     -0.33f, 1.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 1.0f,  // top left -> 4
-    //     -0.33f, -1.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, // bottom left -> 5
-    //     0.33f, 1.0f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f,   // top right -> 6
-    //     0.33f, -1.0f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f,  // bottom right -> 7
-    //     0.33f, 1.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 1.0f,   // top left -> 8
-    //     0.33f, -1.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f,  // bottom left -> 9
-    //     1.00f, 1.0f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f,   // top right -> 10
-    //     1.00f, -1.0f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f,  // bottom right -> 11
-    //     1.00f, 1.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 1.0f,   // top left -> 12
-    //     1.00f, -1.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f,  // bottom left -> 13
-    //     1.66f, 1.0f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f,   // top right -> 14
-    //     1.66f, -1.0f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f,  // bottom right -> 15
-    // };
-    // unsigned int indices[] = {
-    //     0, 1, 3,    // first triangle
-    //     1, 2, 3,    // second triangle
-    //     4, 5, 6,    // Thrird Triangle
-    //     5, 6, 7,    // 4th Triangle
-    //     8, 9, 10,   // 5th Triangle
-    //     9, 10, 11,  // 6th Triangle
-    //     12, 13, 14, // 7th Triangle
-    //     13, 14, 15  // 8th Triangle
-    // };
-    // unsigned int VBO, VAO, EBO;
-    // glGenVertexArrays(1, &VAO);
-    // glGenBuffers(1, &VBO);
-    // glGenBuffers(1, &EBO);
-
-    // glBindVertexArray(VAO);
-
-    // glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    // glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-    // glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    // glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
-    // // position attribute
-    // glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)0);
-    // glEnableVertexAttribArray(0);
-    // // color attribute
-    // glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)(3 * sizeof(float)));
-    // glEnableVertexAttribArray(1);
-    // // texture coord attribute
-    // glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)(6 * sizeof(float)));
-    // glEnableVertexAttribArray(2);
-
-    // // load and create a texture
-    // // -------------------------
-    // unsigned int texture;
-    // glGenTextures(1, &texture);
-    // glBindTexture(GL_TEXTURE_2D, texture); // all upcoming GL_TEXTURE_2D operations now have effect on this texture object
-    // // set the texture wrapping parameters
-    // glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT); // set texture wrapping to GL_REPEAT (default wrapping method)
-    // glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    // // set texture filtering parameters
-    // glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-    // glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    // // load image, create texture and generate mipmaps
-    // int width, height, nrChannels;
-    // char path[100];
-    // strcpy(path, "src/components/background/H2.png");
-    // // The FileSystem::getPath(...) is part of the GitHub repository so we can find files on any IDE/platform; replace it with your own image path.
-    // unsigned char *data = stbi_load(path, &width, &height, &nrChannels, 0);
-    // if (data)
-    // {
-    //     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
-    //     glGenerateMipmap(GL_TEXTURE_2D);
-    // }
-    // else
-    // {
-    //     std::cout << "Failed to load texture" << std::endl;
-    // }
-    // stbi_image_free(data);
-
+    
     back_init();
-
-    int time = 0;
-    int flag = 0;
-
-    int count = 0;
-
-    /*Code For BackGround Ends*/
-    /*Code For Character*/
-    float vertices_1[] = {
-        // Position (x, y, z)  //Colors (arbitrary)  //Textures(x, y)
-        -0.80f, -0.60f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f,
-        -0.80f, -0.93f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f,
-        -0.90f, -0.93f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f,
-        -0.90f, -0.60f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f};
-    unsigned int indices_1[] = {
-        0, 1, 3, // second triangle
-        1, 2, 3 // first triangle
-    };
-
-    unsigned int VBO_1, VAO_1, EBO_1;
-    glGenVertexArrays(1, &VAO_1);
-    glGenBuffers(1, &VBO_1);
-    glGenBuffers(1, &EBO_1);
-
-    glBindVertexArray(VAO_1);
-
-    glBindBuffer(GL_ARRAY_BUFFER, VBO_1);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices_1), vertices_1, GL_STATIC_DRAW);
-
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO_1);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices_1), indices_1, GL_STATIC_DRAW);
-
-    // position attribute
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)0);
-    glEnableVertexAttribArray(0);
-    // color attribute
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)(3 * sizeof(float)));
-    glEnableVertexAttribArray(1);
-    // texture coord attribute
-    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)(6 * sizeof(float)));
-    glEnableVertexAttribArray(2);
-
-    // load and create a texture
-    // -------------------------
-    unsigned int texture_1;
-    glGenTextures(1, &texture_1);
-    glBindTexture(GL_TEXTURE_2D, texture_1); // all upcoming GL_TEXTURE_2D operations now have effect on this texture object
-    // set the texture wrapping parameters
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT); // set texture wrapping to GL_REPEAT (default wrapping method)
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    // set texture filtering parameters
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    // load image, create texture and generate mipmaps
-    // int width, height, nrChannels;
-    char path_1[100];
-    strcpy(path_1, "src/components/character/H1_F.png");
-    // The FileSystem::getPath(...) is part of the GitHub repository so we can find files on any IDE/platform; replace it with your own image path.
-    unsigned char *data_1 = stbi_load(path_1, &width, &height, &nrChannels, 0);
-    if (data_1)
-    {
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data_1);
-        glGenerateMipmap(GL_TEXTURE_2D);
-    }
-    else
-    {
-        std::cout << "Failed to load texture" << std::endl;
-    }
-    stbi_image_free(data_1);
+    character_init();
+    
 
     // render loop
     // ----------- For Level -1 ------------
@@ -199,38 +53,38 @@ int main()
         // bind Texture
         glBindTexture(GL_TEXTURE_2D, texture);
 
-        /*Render The BackGround*/
+        // /*Render The BackGround*/------------------------------------------------------
         glm::mat4 transform = glm::mat4(1.0f); // make sure to initialize matrix to identity matrix first
 
-        if (time < 100)
+        if (new_Time < 100)
         {
             // Create Transformations
-            transform = glm::translate(transform, glm::vec3(float(-0.0033 * time), 0.0f, 0.0f));
-            // std:: cout << "X Coordinate is: " << -0.0033*time << std::endl;
+            transform = glm::translate(transform, glm::vec3(float(-0.0033 * new_Time), 0.0f, 0.0f));
+            // std:: cout << "X Coordinate is: " << -0.0033*new_Time << std::endl;
             // usleep(100);
-            time++;
+            new_Time++;
         }
-        else if(flag == 1)
+        else if (flag == 1)
         {
             transform = glm::translate(transform, glm::vec3(0.0f, 0.0f, 0.0f));
-            time = 0;
+            new_Time = 0;
             count++;
         }
 
-        if(time == 100){
-            flag =1;
+        if (new_Time == 100)
+        {
+            flag = 1;
             usleep(10000);
         }
 
         unsigned int transformLoc = glGetUniformLocation(ourShader.ID, "transform");
         glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(transform));
 
-
         glBindVertexArray(VAO);
         glDrawElements(GL_TRIANGLES, 24, GL_UNSIGNED_INT, 0);
 
-        /*Render BackGround Complete*/
-        /*Render Character*/
+        // /*Render BackGround Complete*/------------------------------------------------------
+        // /*Render Character*/----------------------------------------------------------------
 
         transform = glm::mat4(1.0f);
         transformLoc = glGetUniformLocation(ourShader.ID, "transform");
@@ -240,6 +94,9 @@ int main()
 
         glBindVertexArray(VAO_1);
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        // /*Rendering Character Complete*/----------------------------------------------------------------
+
+
 
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
         // -------------------------------------------------------------------------------
